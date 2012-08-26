60e3faca08a58f3706061b255301a28ba64a9797
Dmitry DA <dimos-d@yandex.ru>
2012-08-27 01:55:31 +0400

    Auth

--- UnitTests/UnitTests.cs
+++ UnitTests/UnitTests.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTests
 {
@@ -11,6 +15,23 @@ namespace UnitTests
 		[TestMethod]
 		public void TestMethod()
 		{
+			var webRequest = WebRequest.Create("http://www.yandex.ru");
+			webRequest.Method = "GET";
+			webRequest.BeginGetResponse(OnRequestCompleted, webRequest);
+			Thread.Sleep(1200000);
+		}
+
+		private void OnRequestCompleted(IAsyncResult ar)
+		{
+			var request = (HttpWebRequest) ar.AsyncState;
+			var response = request.EndGetResponse(ar);
+
+			var responseStream = response.GetResponseStream();
+			if (responseStream != null)
+				using (var stream = new StreamReader(responseStream))
+				{
+					var readToEnd = stream.ReadToEnd();
+				}
 		}
 	}
 }
